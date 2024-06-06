b237c08509ba9b8ab6e3c158ab9e38c08d41d248
Aluno <Aluno@10.85.198.36>
2024-06-06 14:15:32 -0300

    shopping02

---
+++ src/shoppingcard/ShoppingCart.java
@@ -0,0 +1,35 @@
+
+public class ShoppingCard {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        String firstName;
+        int spaceIdx;
+        
+        // Get the index of the space character (" ") in custName. 
+
+
+        // Use the substring method parse out the first name and print it.
+
+
+    }
+
+    
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Aluno
+ * {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
